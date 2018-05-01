AOSiP-Devices/device_huawei_kirin970-common

berkeley: Improve our release tools extension
* Now it's able to detect 8.0 /vendor and remove
  conflicting definitions and labels.

device.mk
@@ -77,6 +77,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION)
 
+# Release tools
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/releasetools.kirin970.sh:install/bin/releasetools.kirin970.sh
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages

omnirom/android_device_huawei_berkeley

Change-Id: I1b7aa00b115b16de71df02b2ddd0c4a9476c132b

device.mk
@@ -91,6 +91,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION)
 
+# Release tools
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/releasetools.kirin970.sh:system/bin/releasetools.kirin970.sh
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages