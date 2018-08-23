3e1864c05cc1b054b1884037686adc23a715f603
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-23 20:29:10 +0200

    berkeley: fixes for Pie
    Change-Id: Idcea8d36253807dc469af9399f3866419812a7c7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,10 @@
 
 include build/make/target/board/generic_arm64_a/BoardConfig.mk
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := berkeley,kirin970
+
+VENDOR_PATH := vendor/huawei/berkeley
 DEVICE_PATH := device/huawei/berkeley
 
 # Platform
@@ -23,45 +27,49 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-PRODUCT_FULL_TREBLE := true
-BOARD_VNDK_VERSION := current
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := berkeley,kirin970
+TARGET_2ND_CPU_VARIANT := cortex-a15
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_NO_KERNEL := false
 TARGET_PREBUILT_KERNEL := /dev/null
 
+TARGET_KERNEL_SOURCE := kernel/huawei/kirin970
+TARGET_KERNEL_CONFIG := merge_kirin970_defconfig
+
+
+ifneq ($(TARGET_AOSP_BASED),)
+PRODUCT_COPY_FILES += \
+        $(TARGET_PREBUILT_KERNEL):kernel
+endif
+
+# Charger
+HEALTHD_ENABLE_HUAWEI_FASTCHG_CHECK := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
-TARGET_KERNEL_SOURCE := kernel/huawei/kirin970
-TARGET_KERNEL_CONFIG := merge_kirin970_defconfig
+# Extended Filesystem Support
+TARGET_EXFAT_DRIVER := exfat
 
 # Partitions
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5767168000
-
-# Camera
-BOARD_USES_SNAPDRAGONCAMERA_VERSION := 2
-
-# Charger
-HEALTHD_ENABLE_HUAWEI_FASTCHG_CHECK := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3707764736
+BOARD_CACHEIMAGE_PARTITION_SIZE := 16777216
 
 # Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Recovery
+ifeq ($(TARGET_AOSP_BASED),)
+BOARD_PROVIDES_BOOTLOADER_MESSAGE := true
+endif
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.kirin970
 
 # Release tools
@@ -73,4 +81,10 @@ TARGET_SYSTEM_AUDIO_EFFECTS := true
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-include vendor/omni/sepolicy/sepolicy.mk
+
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/lib64/libdisplayenginesvc_1_0.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libdisplayenginesvc_1_1.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libhwsmartdisplay_jni.so|libshims_hwsmartdisplay_jni.so \
+    /vendor/bin/hw/vendor.huawei.hardware.hisupl@1.0-service|libshims_hisupl.so
