df51eadf8dd966738cea320c574f3ddc9a311a05
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-26 17:34:37 +0200

    berkeley: pie
    Change-Id: I44eedd2d719c2ae38741daf099afe6fa5c4c4210

--- ril/Android.mk
+++ ril/Android.mk
@@ -14,12 +14,12 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := qti-telephony-common
-LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
-LOCAL_SRC_FILES := $(call all-java-files-under,src)
-LOCAL_MODULE_TAGS := optional
-LOCAL_DEX_PREOPT := false
-include $(BUILD_JAVA_LIBRARY)
+# LOCAL_PATH := $(call my-dir)
+# 
+# include $(CLEAR_VARS)
+# LOCAL_MODULE := qti-telephony-common
+# LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
+# LOCAL_SRC_FILES := $(call all-java-files-under,src)
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_DEX_PREOPT := false
+# include $(BUILD_JAVA_LIBRARY)
