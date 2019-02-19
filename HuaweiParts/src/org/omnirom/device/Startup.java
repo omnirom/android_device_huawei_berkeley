36bbf84e084aa13cd8b70c98064bb1751ffac579
Paul Keith <javelinanddart@gmail.com>
2019-02-19 18:00:38 +0100

    berkeley: Kill DisplayModeControl and related bits
    * This never worked on P
    
    Change-Id: Ib942c1ea6c8ddee1230d0690e2688fb304ec0802

--- HuaweiParts/src/org/omnirom/device/Startup.java
+++ HuaweiParts/src/org/omnirom/device/Startup.java
@@ -31,14 +31,9 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
 
-        context.startService(new Intent(context, ScreenStateService.class));
-
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         SystemProperties.set(DeviceSettings.FPNAV_ENABLED_PROP, sharedPrefs.getBoolean(DeviceSettings.KEY_FP_GESTURES, false) ? "1" : "0");
-        DisplayModeControl.mExtTouchScreen.hwTsSetCoverMode(sharedPrefs.getBoolean(DeviceSettings.KEY_HIGH_TOUCH, false));
-        DisplayModeControl.sDisplayEngineService.setBootComplete(true);
 
-        DisplayModeControl.setMode(Integer.parseInt(Utils.getPreference(context, DeviceSettings.COLOUR_PROFILES_KEY, "0")));
         DisplayModeControl.mHwPowerManager.nativeSetColorTemperature(Integer.parseInt(Utils.getPreference(context, DeviceSettings.COLOUR_TEMP_KEY, "128")));
 
         long packedColor = Color.pack(Integer.parseInt(Utils.getPreference(context, DeviceSettings.COLOUR_TEMP_RGB_KEY,
