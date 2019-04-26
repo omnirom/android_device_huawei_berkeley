1d504b7cd17a78b9043b44d0c371dee68d31eb95
Artem Borisov <dedsa2002@gmail.com>
2019-04-26 21:07:39 +0200

    berkeley: Remove obsolete hacks
    We don't need these on 9.0 vendor.
    
    Change-Id: I2740306faed42aa1f5cb5520b7be5098b803386f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,19 +7,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.kirin970.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.kirin970.ab.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_POST_INSTALL_CMD := \
-    mkdir -p $(PRODUCT_OUT)/system && \
-    ln -sf /system $(PRODUCT_OUT)/system/system
-include $(BUILD_PHONY_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.kirin970.omni.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.kirin970.omni.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
